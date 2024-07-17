93bca301903e2bf400b4f33b447ecdc2793d75e3
enno.weber <enno.weber@stud.uni-goettingen.de>
2024-07-17 04:49:30 +0200

    Fixed train_model, completed test_model

--- bart_detection.py
+++ bart_detection.py
@@ -59,14 +59,15 @@ def transform_data(dataset, max_length=512, tokenizer_name='facebook/bart-large'
     if labels:
         binary_labels = []
         for row in dataset['paraphrase_types']:
-            labels = np.zeros(7, dtype=int)
+            labels = np.zeros(7, dtype=float)
             for i in range(1, 8):
                 if str(i) in row:
                     labels[i - 1] = 1
             binary_labels.append(labels)
         encodings = tokenizer((dataset['sentence1'].to_list()), (dataset['sentence2'].to_list()), truncation=True,
                               padding=True, max_length=256, return_tensors='pt')
-        dataset = TensorDataset(encodings['input_ids'], encodings['attention_mask'], torch.tensor(np.array(binary_labels)))
+        dataset = TensorDataset(encodings['input_ids'], encodings['attention_mask'],
+                                torch.tensor(np.array(binary_labels)))
         data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
         return data_loader
     else:
@@ -76,7 +77,8 @@ def transform_data(dataset, max_length=512, tokenizer_name='facebook/bart-large'
         data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
         return data_loader
 
-def train_model(model, train_data, val_data, device, learning_rate=1e-5, epochs=3):
+
+def train_model(model, train_data, val_data, device, learning_rate=1e-5, epochs=3, output_dir="output"):
     """
     Train the model. You can use any training loop you want. We recommend starting with
     AdamW as your optimizer. You can take a look at the SST training loop for reference.
@@ -90,13 +92,14 @@ def train_model(model, train_data, val_data, device, learning_rate=1e-5, epochs=
     # Loss Function and Optimizer
     loss_fn = torch.nn.CrossEntropyLoss()
     optimizer = AdamW(model.parameters(), lr=learning_rate)
-    best_dev_acc = float("-inf")
+    best_val_loss = float("inf")
     for epoch in range(epochs):
         print(f"Epoch {epoch + 1}/{epochs}")
+
         train_loss = 0
         num_batches = 0
         model.train()
-        for batch in tqdm(train_data, desc=f"train-{epoch+1:02}", disable=TQDM_DISABLE):
+        for batch in tqdm(train_data, desc=f"train-{epoch + 1:02}"):
             b_ids, b_mask, b_labels = (
                 batch[0],
                 batch[1],
@@ -107,31 +110,40 @@ def train_model(model, train_data, val_data, device, learning_rate=1e-5, epochs=
             b_labels = b_labels.to(device)
 
             optimizer.zero_grad()
-            logits = model(input_ids=b_ids, attention_mask=b_mask)
-            loss = F.cross_entropy(logits, b_labels.view(-1))
+            outputs = model(input_ids=b_ids, attention_mask=b_mask)
+            loss = loss_fn(outputs, b_labels)
             loss.backward()
             optimizer.step()
 
             train_loss += loss.item()
             num_batches += 1
         train_loss = train_loss / num_batches
+        accuracy = evaluate_model(model=model, test=train_data, device=device)
+        print(f"Train Accuracy: {accuracy}")
         print(f"Train loss: {train_loss}")
         val_loss = 0
         model.eval()
         with torch.no_grad():
-            for batch in tqdm(val_data, desc="Validation"):
-                input_ids, attention_mask, labels= batch
-                input_ids = input_ids.to(device)
-                attention_mask = attention_mask.to(device)
+            for batch in tqdm(val_data, desc=f"Validation"):
+                b_ids, b_mask, b_labels = (
+                    batch[0],
+                    batch[1],
+                    batch[2],
+                )
+                b_ids = b_ids.to(device)
+                b_mask = b_mask.to(device)
+                b_labels = b_labels.to(device)
 
-                outputs = model(input_ids=input_ids, attention_mask=attention_mask)
-                loss = loss_fn(outputs.logits.view(-1, outputs.logits.shape[-1]), input_ids.view(-1))
-                val_loss += loss.item()
+            outputs = model(input_ids=b_ids, attention_mask=b_mask)
+            loss = loss_fn(outputs, b_labels)
+            val_loss += loss.item()
 
         val_loss = val_loss / len(val_data)
         print(f"Validation loss: {val_loss}")
-
-
+        if val_loss < best_val_loss:
+            best_val_loss = val_loss
+            # Save the model
+            model.save_pretrained(output_dir)
 
 
 def test_model(model, test_data, test_ids, device):
@@ -141,12 +153,25 @@ def test_model(model, test_data, test_ids, device):
     The 'Predicted_Paraphrase_Types' column should contain the binary array of your model predictions.
     Return this dataframe.
     """
-    ### TODO
     model.eval()
     paraphrase_types = []
-    with torch.no_grad():
-
-
+    ids = []
+    for batch in tqdm(test_data, desc=f"Testing"):
+        b_ids, b_mask = (
+            batch[0],
+            batch[1],
+        )
+        b_ids = b_ids.to(device)
+        b_mask = b_mask.to(device)
+        outputs = model(input_ids=b_ids, attention_mask=b_mask)
+        predicted_labels = (outputs > 0.5).int()
+        paraphrase_types.append(np.array(predicted_labels))
+        ids.append(np.array(b_ids))
+    df = pd.DataFrame({
+        'id' : ids,
+        'Predicted_Paraphrase_Types' : paraphrase_types
+    })
+    return df
 def evaluate_model(model, test_data, device):
     """
     This function measures the accuracy of our model's prediction on a given train/validation set
@@ -217,7 +242,7 @@ def finetune_paraphrase_detection(args):
 
     train_dataset = pd.read_csv("data/etpc-paraphrase-train.csv", sep="\t")
     test_dataset = pd.read_csv("data/etpc-paraphrase-detection-test-student.csv", sep="\t")
-    #split data
+    # split data
     train = train_dataset.sample(frac=0.8)
     val_dataset = train_dataset.drop(train.index)
     val_data = transform_data(val_dataset)
