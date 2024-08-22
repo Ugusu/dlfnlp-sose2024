83bd1857a84182daabc0c2fab55d7bae66aed33d
darizae <danielarizae@gmail.com>
2024-08-22 17:18:22 +0200

    47: Fix error with working dirs

--- grid_search/experiment_scripts/submit_grid_search_jobs.sh
+++ grid_search/experiment_scripts/submit_grid_search_jobs.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Submit the scripts using sbatch
-sbatch run_grid_search_extra_layer_non_regularized.sh
-sbatch run_grid_search_extra_layer_regularized.sh
-sbatch run_grid_search_no_extra_layer_non_regularized.sh
-sbatch run_grid_search_no_extra_layer_regularized.sh
+sbatch ./experiment_scripts/run_grid_search_extra_layer_non_regularized.sh
+sbatch ./experiment_scripts/run_grid_search_extra_layer_regularized.sh
+sbatch ./experiment_scripts/run_grid_search_no_extra_layer_non_regularized.sh
+sbatch ./experiment_scripts/run_grid_search_no_extra_layer_regularized.sh
 
 echo "All scripts have been submitted."
