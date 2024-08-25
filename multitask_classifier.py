4678b339202844261fa0046dc873c8c2434cf045
Ugusu <ugur.mamedzade@gmail.com>
2024-08-25 11:42:39 +0200

    SMART v2 bugfix 5

--- multitask_classifier.py
+++ multitask_classifier.py
@@ -425,7 +425,6 @@ def train_multitask(args):
                 logits = model.predict_paraphrase(b_ids_1, b_mask_1, b_ids_2, b_mask_2)
                 bce_with_logits_loss = nn.BCEWithLogitsLoss()
                 loss = bce_with_logits_loss(logits.squeeze(), b_labels.float())
-                loss.backward()
 
                 # Add SMART regularization
                 if smart_regularizer:
@@ -449,9 +448,11 @@ def train_multitask(args):
                         F.softmax(perturbed_logits.detach(), dim=-1)
                     )
 
-                    smart_loss.backward()
+                    loss += smart_loss
 
+                loss.backward(retrain_graph=True)
                 optimizer.step()
+
                 train_loss += loss.item()
                 num_batches += 1
 
