95144838a80e39874751d7651595e7858c2eaea0
enno.weber <enno.weber@stud.uni-goettingen.de>
2024-08-26 21:14:53 +0200

    update run_train.sh

--- run_train.sh
+++ run_train.sh
@@ -33,7 +33,7 @@ echo -e "Uncommitted Changes: $(git status --porcelain | wc -l)\n"
 
 # Run the script:
 # This is the default training command; change to specific task and desired hyperparameters
-python bart_detection.py --use_gpu --batch_size=96 --lr=0.0001 --epochs=10 --weight_decay=0.01 --optimizer=AdamW
+python bart_detection.py --use_gpu --batch_size=96 --lr=0.0001 --epochs=10 --weight_decay=0.001 --optimizer=AdamW
 
 # Check live logs (replace <jobid> with your actual job ID)
 # tail -f slurm_files/slurm-train-bart-detection-<jobid>.out
